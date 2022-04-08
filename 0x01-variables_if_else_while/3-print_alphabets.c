eccf660da9abd20f14d2bc470cdf3acc1c5a6831
gideonopa <gideonoparinde@gmail.com>
2022-04-08 16:55:59 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print the lowercase and uppercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
+
+putchar('\n');
+
+return (0);
+}
