a0e90ea959cbbaf5a55cdcfcdd119b6eeb8270a0
gideonopa <gideonoparinde@gmail.com>
2022-04-08 17:13:52 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print number 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+
+for (a = 0; a < 10; a++)
+putchar(a);
+
+putchar('\n');
+
+return (0);
+}
