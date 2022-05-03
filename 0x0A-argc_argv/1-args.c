995b74d2dc45a8eb1ba3c2202f8d1b38c36326dd
gideonopa <gideonoparinde@gmail.com>
2022-05-03 01:30:52 +0000

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print the number of arguments passed
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; //Ignore argv
+printf("%i\n", argc - 1);
+
+return (0);
+}
