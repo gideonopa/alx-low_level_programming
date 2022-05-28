9e48968a0c88bc7b1b41f037dcd3ba416e3f769f
gideonopa <gideonoparinde@gmail.com>
2022-05-28 01:19:30 +0000

    More linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t numb = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		numb++;
 		h = h->next;
+		numb++;
 	}
 	return (numb);
 }
